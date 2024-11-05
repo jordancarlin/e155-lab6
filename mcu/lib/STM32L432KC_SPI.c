bf1ece4951e5a45df21ec21bfc1c047e30a329ae
Jordan Carlin <jordanmcarlin@gmail.com>
2024-11-05 15:44:28 -0800

    Implement SPI library

--- mcu/lib/STM32L432KC_SPI.c
+++ mcu/lib/STM32L432KC_SPI.c
@@ -4,13 +4,59 @@
 // 11/4/2024
 // source code for SPI functions
 
-/* Enables the SPI peripheral and intializes its clock speed (baud rate), polarity, and phase.
+#include "STM32L432KC_SPI.h"
+#include "STM32L432KC_RCC.h"
+#include "STM32L432KC_GPIO.h"
+
+/* Enables the SPI peripheral and initializes its clock speed (baud rate), polarity, and phase.
  *    -- br: (0b000 - 0b111). The SPI clk will be the master clock / 2^(BR+1).
  *    -- cpol: clock polarity (0: inactive state is logical 0, 1: inactive state is logical 1).
  *    -- cpha: clock phase (0: data captured on leading edge of clk and changed on next edge,
  *          1: data changed on leading edge of clk and captured on next edge)
  * Refer to the datasheet for more low-level details. */
 void initSPI(int br, int cpol, int cpha){
+  // enable GPIO pins
+  gpioEnable(GPIO_PORT_A);
+  gpioEnable(GPIO_PORT_B);
+
+  pinMode(SPI_CE, GPIO_OUTPUT);
+  pinMode(SPI_SCK, GPIO_ALT);
+  pinMode(SPI_MOSI, GPIO_ALT);
+  pinMode(SPI_MISO, GPIO_ALT);
+  GPIOB->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL3, 5);
+  GPIOB->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL4, 5);
+  GPIOB->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL5, 5);
+
+  // Enable SPI system clock
+  RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+
+  // Configure SPI
+  // CR1 configuration
+  // set baud rate
+  SPI1->CR1 |= _VAL2FLD(SPI_CR1_BR, br);
+  // set clock polarity
+  SPI1->CR1 |= _VAL2FLD(SPI_CR1_CPOL, cpol);
+  // set clock phase
+  SPI1->CR1 |= _VAL2FLD(SPI_CR1_CPHA, cpha);
+  // Leave simplex/duplex at default for now
+  // Leave frame format at default
+  // Don't set crc integrity checking
+  // Leave SSM and SSI at default
+  // set as SPI controller
+  SPI1->CR1 |= (SPI_CR1_MSTR);
+
+  // CR2 configuration
+  // set to 8-bit packets
+  SPI1->CR2 |= _VAL2FLD(SPI_CR2_DS, 0b0111);
+
+  // set FIFO full length to 8
+  SPI1->CR2 |= (SPI_CR2_FRXTH);
+
+  // Enable peripheral select mode
+  SPI1->CR2 |= (SPI_CR2_SSOE);
+
+  // Enable SPI system
+  SPI1->CR1 |= (SPI_CR1_SPE);
 
 }
 
@@ -18,5 +64,13 @@ void initSPI(int br, int cpol, int cpha){
  *    -- send: the character to send over SPI
  *    -- return: the character received over SPI */
 char spiSendReceive(char send){
-
+  // wait until transmit FIFO is empty (1 when empty)
+  while(!(SPI1->SR & SPI_SR_TXE));
+  // write data to transmit FIFO
+  SPI1->DR = send;
+  // wait until receive FIFO is NOT empty (0 when empty)
+  while(!(SPI1->SR & SPI_SR_RXNE));
+  // read data from receive FIFO
+  char receivedData = SPI1->DR;
+  return receivedData;
 }
